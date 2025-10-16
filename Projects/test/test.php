f9d8595685a894194caa82743591e3b391c45f01
Md. Asadujjaman Sabuj <49393707+asadujjamann@users.noreply.github.com>
2025-10-16 10:13:27 +0600

    update

---
+++ Projects/test/test.php
@@ -0,0 +1,28 @@
+<!-- 
+ 
+<em>Copyright &copy; *|CURRENT_YEAR|* *|LIST:COMPANY|*, All rights reserved.</em><br />
+*|IFNOT:ARCHIVE_PAGE|* *|LIST:DESCRIPTION|*<br />
+<br />
+<strong>Our mailing address is:</strong><br />
+*|HTML:LIST_ADDRESS_HTML|* *|END:IF|*<br />
+<br />
+Want to change how you receive these emails?<br />
+You can <a href="*|UPDATE_PROFILE|*">update your preferences</a> or <a href="*|UNSUB|*">unsubscribe from this list</a>.<br />
+<br />
+*|IF:REWARDS|* *|HTML:REWARDS|* *|END:IF|*
+
+
+
+
+Copyright © *|CURRENT_YEAR|* *|LIST:COMPANY|*, All rights reserved.
+*|IFNOT:ARCHIVE_PAGE|* *|LIST:DESCRIPTION|*
+
+Our mailing address is:
+*|HTML:LIST_ADDRESS_HTML|* *|END:IF|*
+
+Want to change how you receive these emails?
+You can update your preferences or unsubscribe from this list.
+
+*|IF:REWARDS|* *|HTML:REWARDS|* *|END:IF|*
+
+-->
